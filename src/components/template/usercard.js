5925ebc7e478657be9e755c7b47b12241d52408d
Jose Samuel Reyes Rivas <jose.reyes@hipotecario.com.sv>
2021-05-26 11:37:13 -0600

    Adding de template structure

---
+++ src/components/template/usercard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export class UserCard extends React.Component {
+
+
+    render (){
+        return <div>
+            
+            <div ><h3 > JSR </h3></div>
+        </div>
+    }
+    
+}
