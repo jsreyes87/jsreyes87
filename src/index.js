45b3355f4ec09bb28ec565412d66645c5d80ab4e
JSRR <samuel.js87@gmail.com>
2021-05-29 18:22:32 -0600

    Adding all props as parameters for component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-
-            <CommentDetail author="Joahn"/>
-            <CommentDetail author="Josh"/>
-            <CommentDetail author="Laura"/>
-            <CommentDetail author="Axel"/>
+            <CommentDetail author="Sam" timeAgo="Monday at 4:30PM" comment="Hi.!! There " image={faker.image.avatar()} />
+            <CommentDetail author="Joahn" timeAgo="Today at 3:30PM" comment="Comment something!!" image={faker.image.avatar()} />
+            <CommentDetail author="Josh" timeAgo="Yesterday at 10:30AM" comment="Blessing!!!" image={faker.image.avatar()} />
+            <CommentDetail author="Laura" timeAgo="Today at 4:20PM" comment="Hi.!!" image={faker.image.avatar()} />
+            <CommentDetail author="Axel" timeAgo="Today at 4:30PM" comment="Hi.!!" image={faker.image.avatar()} />
         </div>
     );
 }
